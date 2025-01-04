6aae3eca3a1b6548e1a795d2104da8e94ef16c74
Moreshwar Wagh <moreshwarswagh@gmail.com>
2025-01-04 23:06:51 +0530

    feat: update TaskList component with new layout and task details

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,27 +2,78 @@ import React from "react";
 
 const TaskList = () => {
   return (
-    <div id="tasklist" className="h-[45%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10">
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
+    <div
+      id="tasklist"
+      className="h-[45%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10"
+    >
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a yt video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quibusdam
+          placeat! Laboriosam est, maxime facere odio recusandae non, magni
+          rerum nobis repellendus, aliquid dicta error impedit? Nam nesciunt ut
+          incidunt.
+        </p>
       </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
+
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a yt video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quibusdam
+          placeat! Laboriosam est, maxime facere odio recusandae non, magni
+          rerum nobis repellendus, aliquid dicta error impedit? Nam nesciunt ut
+          incidunt.
+        </p>
       </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
+
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a yt video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quibusdam
+          placeat! Laboriosam est, maxime facere odio recusandae non, magni
+          rerum nobis repellendus, aliquid dicta error impedit? Nam nesciunt ut
+          incidunt.
+        </p>
       </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
+
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-purple-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a yt video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quibusdam
+          placeat! Laboriosam est, maxime facere odio recusandae non, magni
+          rerum nobis repellendus, aliquid dicta error impedit? Nam nesciunt ut
+          incidunt.
+        </p>
       </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
-      </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
-      </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
-        <h1>Task List</h1>
+
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">20 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a yt video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quibusdam
+          placeat! Laboriosam est, maxime facere odio recusandae non, magni
+          rerum nobis repellendus, aliquid dicta error impedit? Nam nesciunt ut
+          incidunt.
+        </p>
       </div>
     </div>
   );
